d3d6225dc54065986069a3864b0cf0081ab965d5
Giovanni <52502639+Giovino69@users.noreply.github.com>
2019-07-04 13:25:19 +0200

    Add files via upload

---
+++ src/app/products.ts
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    photo:"https://assets.mspimages.in/wp-content/uploads/2019/01/Screenshot-2019-01-16-at-1.39.05-PM.png"
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    photo:"https://media.melablog.it/7/77d/1.jpg"
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: '',
+    photo:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone8/plus/iphone8-plus-gold-select-2018?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1550795417455"
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
